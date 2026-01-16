8ca8d94e2c8ce4f37156f79b3d3f78dd72b0ff81
Emmanuel A. <alabiemmanuel2937@gmail.com>
2026-01-16 16:26:49 +0200

    deployment issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel"; 
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server", // important for API routes
+  adapter: vercel(),
   vite: {
     server: {
       allowedHosts: ['lianoid-aracelis-ungroaning.ngrok-free.dev']
