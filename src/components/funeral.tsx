ef9f334c47826794cdc9c78a050b2263d29551b9
Emmanuel A <alabiemmanuel2937@gmail.com>
2026-02-19 21:11:03 +0200

    Update funeral.tsx

--- src/components/funeral.tsx
+++ src/components/funeral.tsx
@@ -173,7 +173,7 @@ export default function Funeral(): JSX.Element {
                             </div>
                         ) : (
                             <div className="w-full h-full flex items-center justify-center bg-neutral-900 text-white text-lg font-semibold">
-                                The livestream for Service of Songs will appear here. Stay tuned!
+                                The livestream for Funeral Service will appear here tommorow. Stay tuned!
                             </div>
                         )}
                     </div>
@@ -187,6 +187,7 @@ export default function Funeral(): JSX.Element {
 <iframe width="990" height="557" src="https://www.youtube.com/embed/Fw9hgttWzIg" title="🔴 Crystal Bay Beach Resort | Lamai | Koh Samui | Thailand | Live Beach Webcam | 2160p 4K" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
 */
+
 
 
 
